c682e238d5f70aae4dbc322df88d7f0d220b8ee3
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-13 00:10:05 -0800

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,1 +1,10 @@
-foot
+// src/components/Footer.js
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <p>&copy; 2025 Palace of Goods. All Rights Reserved.</p>
+  </footer>
+);
+
+export default Footer;
