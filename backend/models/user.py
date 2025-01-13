a2aee0a09cbe4113d5b6f2fc54077d2d2766a371
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-12 23:43:03 -0800

    Update user.py

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,6 +1,9 @@
 from . import db
 
 class User(db.Model):
+    role = db.Column(db.String(50), default="user")  # Default is 'user'
+
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
