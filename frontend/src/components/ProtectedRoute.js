96a46e4d30c7b0e0fece2202c73e1a8119d150a1
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-13 00:03:59 -0800

    Update ProtectedRoute.js

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -1,1 +1,12 @@
-t
+// src/components/ProtectedRoute.js
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+
+const ProtectedRoute = ({ children }) => {
+  const { token } = useContext(AuthContext);
+
+  return token ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
