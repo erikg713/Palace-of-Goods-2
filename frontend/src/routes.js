e08bb0a0a76d545d92ce301c91adfa3111611f27
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-13 12:31:10 -0800

    Update routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,1 +1,23 @@
-s
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Products from './components/Products';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+import NotFound from './components/NotFound';
+
+const Routes = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/products" component={Products} />
+                <Route path="/cart" component={Cart} />
+                <Route path="/checkout" component={Checkout} />
+                <Route component={NotFound} />
+            </Switch>
+        </Router>
+    );
+};
+
+export default Routes;
