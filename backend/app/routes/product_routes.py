7fb96af9051b897f5edb6d90ddb5fd72987e4cab
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-16 10:53:02 -0600

    Update product_routes.py

--- backend/app/routes/product_routes.py
+++ backend/app/routes/product_routes.py
@@ -1,1 +1,7 @@
+from flask import Blueprint
+from app.controllers.product_controller import add_product, get_products
 
+product_bp = Blueprint('product', __name__)
+
+product_bp.route('/', methods=['POST'])(add_product)
+product_bp.route('/', methods=['GET'])(get_products)
