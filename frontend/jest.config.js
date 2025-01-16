080ee077b38844758872a935080374e34531e8ff
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-16 09:32:35 -0600

    Delete frontend directory

--- frontend/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'jest-preset-angular',
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-};
