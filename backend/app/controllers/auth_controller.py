c7bcd1725244fd8fadfb428c2be94e7cabd21e6c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-16 10:51:28 -0600

    Update auth_controller.py

--- backend/app/controllers/auth_controller.py
+++ backend/app/controllers/auth_controller.py
@@ -1,1 +1,31 @@
+from flask import request, jsonify
+from app.models.user_model import User
+from app import db
+from flask_jwt_extended import create_access_token
 
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    if User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first():
+        return jsonify({'message': 'User already exists'}), 400
+
+    user = User(username=username, email=email)
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'message': 'User registered successfully'}), 201
+
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        access_token = create_access_token(identity={'id': user.id, 'username': user.username, 'is_admin': user.is_admin})
+        return jsonify({'access_token': access_token}), 200
+
+    return jsonify({'message': 'Invalid credentials'}), 401
