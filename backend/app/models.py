084683a313ffb341fc904ca57802a9005d8b687f
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-18 05:45:13 -0600

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,13 @@
+from app import db
 
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+        }
