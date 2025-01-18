57183f8e47491f1f396099ef48397a979fd66279
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-18 05:41:25 -0600

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost:5432/palace_of_goods')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY")
