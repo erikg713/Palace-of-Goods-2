bf33f5f000d470f85141d7d3351646a5f1b603bf
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-19 05:09:25 -0600

    Update service-worker.js

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -37,3 +37,19 @@ self.addEventListener("activate", (event) => {
     )
   );
 });
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/static/js/main.js",
+  "/static/css/main.css",
+  "/favicon.ico",
+  "/offline.html",
+];
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => caches.match("/offline.html"));
+    })
+  );
+});
