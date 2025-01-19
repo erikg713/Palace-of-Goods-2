8077a208ecf940e67225a9abf16a53674a7bfda2
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-19 12:43:53 -0600

    Update and rename run.py to App.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/run.py
+++
@@ -1,9 +0,0 @@
-from app import create_app, db
-from app.models import Product
-import os
-
-app = create_app()
-
-if __name__ == '__main__':
-    debug_mode = os.getenv('FLASK_DEBUG', 'False').lower() in ['true', '1', 't']
-    app.run(debug=debug_mode)
