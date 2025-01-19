c415759662c9915bc81cfe72dd3fa5e39b3e63c1
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-19 12:15:19 -0600

    Update index.ts
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/Shop/index.ts
+++ frontend/src/Shop/index.ts
@@ -1,4 +1,23 @@
-// frontend/src/Shop/index.ts
+// frontend/src/shop/index.ts
+
+const signIn = async () => {
+  const scopes = ["username", "payments"];
+  const authResponse = await window.Pi.authenticate(scopes, onIncompletePaymentFound);
+
+  /* pass obtained data to backend */
+  await signInUser(authResponse);
+
+  /* use the obtained data however you want */
+  setUser(authResponse.user);
+};
+
+const signInUser = (authResult: any) => {
+  axiosClient.post("/signin", { authResult }, config);
+
+  return setShowModal(false);
+};
+
+
 
 const onIncompletePaymentFound = (payment: PaymentDTO) => {
   console.log("onIncompletePaymentFound", payment);
