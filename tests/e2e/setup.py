696ee4a4a52c31bcff5c6dc6e319c9efa08e19d2
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-20 13:24:53 -0600

    Update setup.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- tests/e2e/setup.py
+++ tests/e2e/setup.py
@@ -1,1 +1,7 @@
+# tests/e2e/setup.py
+import subprocess
 
+def start_backend():
+    """Start the backend service for E2E tests."""
+    process = subprocess.Popen(["flask", "run"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return process
