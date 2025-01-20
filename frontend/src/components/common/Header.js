7c4e88dfd7918b4162ef3f2ea00d907f82bc92c3
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-19 19:15:46 -0600

    Update Header.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -1,1 +1,24 @@
+// src/components/Common/Header.js
 
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';  // Importing CSS for styling
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">Palace of Goods</Link>
+      </div>
+      <nav className="nav">
+        <ul>
+          <li><Link to="/products">Products</Link></li>
+          <li><Link to="/cart">Cart</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
