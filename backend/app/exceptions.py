f8ad889b820aca3f2deea1f4b7a85f7f963fde34
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-19 22:56:36 -0600

    Update exceptions.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/app/exceptions.py
+++ backend/app/exceptions.py
@@ -1,1 +1,11 @@
+# backend/app/exceptions.py
 
+class AppError(Exception):
+    def __init__(self, message, status_code):
+        super().__init__(message)
+        self.message = message
+        self.status_code = status_code
+
+class ProductNotFoundError(AppError):
+    def __init__(self):
+        super().__init__("Product not found", 404)
