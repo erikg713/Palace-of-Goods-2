de0fec0fb2c7be0e00f022c3fbfb60bc08216de7
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-20 03:18:44 -0600

    Update commands.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- Cypress/support/commands.js
+++ Cypress/support/commands.js
@@ -1,6 +1,23 @@
 Cypress.Commands.add('login', (username, password) => {
-  cy.visit('/login');
-  cy.get('input[name="username"]').type(username);
-  cy.get('input[name="password"]').type(password);
-  cy.get('button[type="submit"]').click();
+  cy.request('POST', `${Cypress.env('apiUrl')}/auth/login`, {
+    username,
+    password
+  }).then((response) => {
+    localStorage.setItem('token', response.body.access_token);
+  });
+});
+
+Cypress.Commands.add('addProduct', (name, description, price) => {
+  cy.request({
+    method: 'POST',
+    url: `${Cypress.env('apiUrl')}/products`,
+    body: { name, description, price },
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`
+    }
+  });
+});
+before(() => {
+  cy.login('testuser', 'testpassword');
+  cy.addProduct('Sample Product', 'This is a test product', 100);
 });
