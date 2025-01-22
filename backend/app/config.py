e2c566be90e09b58c5ae77c12eeda8ee3afc909a
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-22 10:43:26 -0600

    Update config.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,1 +1,6 @@
+import os
 
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///palace_of_goods.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
