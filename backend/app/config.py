16fd517b16a58a3d3abb88d5c9862ecb1d6653dd
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-22 11:54:48 -0600

    Update config.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # Load variables from .env
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///palace_of_goods.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
+    PI_API_KEY = os.getenv("PI_API_KEY")
