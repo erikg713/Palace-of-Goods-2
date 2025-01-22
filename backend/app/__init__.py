20e395c8a75d4e5a9063636ebf03015e0137327d
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-22 10:43:03 -0600

    Update __init__.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,1 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
 
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    
+    # Load configurations
+    app.config.from_object("app.config.Config")
+
+    # Initialize extensions
+    db.init_app(app)
+
+    # Register routes
+    with app.app_context():
+        from . import routes
+        db.create_all()
+
+    return app
