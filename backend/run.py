61bacaaf0bba9035d065869f27d128d58b341b1e
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-22 13:21:56 -0600

    Update run.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/run.py
+++ backend/run.py
@@ -4,3 +4,10 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
+from flask import Flask
+from app.routes import bp as api_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(api_bp)
+    return app
