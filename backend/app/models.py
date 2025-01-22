7a8ea4bd2020a8379c2ff9630a1d341053e02840
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-22 10:44:20 -0600

    Update models.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,15 @@
+from app import db
 
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    price = db.Column(db.Float, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+        }
