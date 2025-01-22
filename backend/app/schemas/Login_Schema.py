b2b7afdcd6e5919e0b35a335f23d8e3af079755c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-22 16:23:32 -0600

    Update Login_Schema.py
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/app/schemas/Login_Schema.py
+++ backend/app/schemas/Login_Schema.py
@@ -1,1 +1,5 @@
+from marshmallow import Schema, fields
 
+class LoginSchema(Schema):
+    username = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)  # Don't expose in output
