66071d8ce2703d98dec596581676e97637fab455
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-21 23:20:27 -0600

    Delete tests directory
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- tests/e2e/teardown.py
+++
@@ -1,5 +0,0 @@
-# tests/e2e/teardown.py
-def stop_backend(process):
-    """Stop the backend service."""
-    process.terminate()
-    process.wait()
--- tests/user.test.js
+++
@@ -1,7 +0,0 @@
-// user.test.js
-import { createUser } from './User';
-
-test('should create a user object', () => {
-  const user = createUser('John', 30);
-  expect(user).toEqual({ name: 'John', age: 30 });
-});
