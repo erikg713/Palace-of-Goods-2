2f519ad22de289c0981e2ea744cf808283a4a316
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-22 19:36:33 -0600

    Create ProtectedRoute.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ frontend/src/components/ProtectedRoute.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
