5580b7b3b62615432a4a5b160af0adbfa3225f01
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-23 02:57:47 -0600

    Create About.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Palace of Goods</h2>
+      <p>
+        Palace of Goods is a Web3-powered marketplace where users can buy, sell,
+        and trade products seamlessly.
+      </p>
+    </div>
+  );
+};
+
+export default About;
