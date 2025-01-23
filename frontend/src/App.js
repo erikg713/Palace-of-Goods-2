192afc29f85626e394f85580296f4fc3f75cb6bc
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-23 02:51:31 -0600

    Update App.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
 import { AuthProvider } from "./context/AuthContext";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Marketplace from './components/Marketplace';
 
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Marketplace />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
 function App() {
   return (
     <AuthProvider>
