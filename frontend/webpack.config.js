cc1ff38ae209e357071c0023afb7ee855b3b691c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-01-23 18:08:54 -0600

    Create webpack.config.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ frontend/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    mode: 'development',
+    devServer: {
+        static: './dist',
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+};
